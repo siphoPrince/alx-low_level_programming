a42540c84f879c577470333514e0ab2648d71cfc
Sipho <mabirimisesipho@gmail.com>
2023-02-16 11:54:28 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main - Print code using puts
+/**
+ * main -print a line of code cod using puts
  *
  * return: 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
