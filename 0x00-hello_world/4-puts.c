79890696977ff1b063fe5e6350262c8ad345f8b1
Sipho <mabirimisesipho@gmail.com>
2023-02-16 11:40:26 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/*
+ * main - Print code using puts
+ *
+ * return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
