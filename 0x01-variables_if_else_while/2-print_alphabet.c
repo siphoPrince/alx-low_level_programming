edd36d6fffaa8d5d38cf1bfa3afe4b3abc68fd4f
Sipho <mabirimisesipho@gmail.com>
2023-02-17 07:15:57 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing in lower case
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n')
+	return (0);
+}
