657098870ab72e04f29570da0eeb9bdd9e472c48
Sipho <mabirimisesipho@gmail.com>
2023-02-17 05:38:59 -0800

    Push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,12 +20,12 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 
-	if (n == 0)
+	else if (n == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 
-	if (n < 6)
+	else (n < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
