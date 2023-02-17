de0cb886afed1f858b7170855a10722fb9b118f3
Sipho <mabirimisesipho@gmail.com>
2023-02-17 05:42:10 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-last_digit.c -o 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,9 +25,10 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
 
-	else (n < 6)
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 
 	return (0);
+}
