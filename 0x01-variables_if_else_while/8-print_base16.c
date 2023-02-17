03def6261dd33c4ce9484b9144c453a55a9655fa
Sipho <mabirimisesipho@gmail.com>
2023-02-17 09:21:17 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+*/
+
+int main(void)
+{
+	char c;
+	int d;
+
+	c = 'a';
+	d= 0;
+	while
+		(d < 10) {
+			putchar(d + 'a');
+			d++
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
+}
