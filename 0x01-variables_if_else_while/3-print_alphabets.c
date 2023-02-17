212fceb63cbfecc0093aed0c673cfbb4e70640e6
Sipho <mabirimisesipho@gmail.com>
2023-02-17 07:51:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main-  prints the alphabet in lowercase
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int l;
+
+	for (l = 0; l < 26; l++)
+	{
+		putchar(alphabet[l]);
+	}
+	putchar('\n');
+	return (0);
+}
