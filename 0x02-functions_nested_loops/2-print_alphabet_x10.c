e186ef447c6dab739250fb9756d47054828456b1
Sipho <mabirimisesipho@gmail.com>
2023-02-21 07:04:58 -0800

    alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print 10x
+*Return: 0
+*/
+void print_alphabet_x10(void)
+{
+
+	char ch;
+		for (ch = 'a'; ch <= 'z'; ch < 10);
+		ch++;
+		_putchar(ch);
+	putchar('\n');
+}
