db537b1691d9686c052a02dd3788ce1846f0e266
Sipho <mabirimisesipho@gmail.com>
2023-02-21 08:46:01 -0800

    check alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_isalpha - checking for alphabets
+ *@c - element
+ *Return: if true 0 if false 1
+*/
+
+int _isalpha(int c)
+{
+	if (isalpha('c'))
+		return (1);
+	else
+		return (0);
+}
