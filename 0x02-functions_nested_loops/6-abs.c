271ee2a5a35542836ad9eb0c2f481b6e2781da39
Sipho <mabirimisesipho@gmail.com>
2023-02-21 09:47:29 -0800

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _abs - computes the absolute value
+ * @ch: The integer ro be computed
+ * Return: the value commited
+ */
+
 int _abs(int ch)
 {
 	if (ch >= 0)
