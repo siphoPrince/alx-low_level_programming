3034fe5a10c4a950fe8650cb087c4a56c49d9295
Sipho <mabirimisesipho@gmail.com>
2023-02-21 11:23:47 -0800

    times table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,9 @@ void times_table(void)
 
 				if (prod <= 9)
 					_putchar(' ');
-				else 
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
+					else 
+						_putchar((prod / 10) + '0');
+						_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
