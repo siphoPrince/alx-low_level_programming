4472add803f2151d910a2333f0d83949416ade68
Sipho <mabirimisesipho@gmail.com>
2023-02-21 01:48:01 -0800

    Alpahbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Write a function that prints the alphabet
+ * Return: 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z" ; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
