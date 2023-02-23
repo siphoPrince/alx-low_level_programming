43fe190608aec6d8970356f648bde35233ea1eb5
Sipho <mabirimisesipho@gmail.com>
2023-02-23 04:09:17 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,15 @@
 /**
  * _isupper - changing the upper case
  * Return: if is upper return 1 if its not return 0
+ * @c: the number of checker
 */
 
 int _isupper(int c)
 {
-	if (c <= 'c' && c >= 'z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
