c439b56af7f782d8f1813f2b570e6ab0bc2083c6
Sipho <mabirimisesipho@gmail.com>
2023-02-23 03:27:13 -0800

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - changing the upper case
+ * Return: if is upper return 1 if its not return 0
+*/
+
+int _isupper(int c)
+{
+	if (isupper('c'))
+		_putchar(c);
+	return (1);
+	else
+		return (0);
+}
