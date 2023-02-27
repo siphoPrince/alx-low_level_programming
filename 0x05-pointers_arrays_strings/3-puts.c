0f3ed5f2bd685b841582e9ffd6d99736c29a9eec
Sipho <mabirimisesipho@gmail.com>
2023-02-27 08:26:25 -0800

    3 print

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ *
+ * Return: 0
  */
+
 void _puts(char *str)
 {
+	int i = 0;
 
-	while (*str != '\0')
+	while (str[i])
 	{
-		_putchar(*str++);
-		
+		_putchar(str[i]);
+		i++;
 	}
-		_putchar('\n');
+	_putchar('\n');
 }
