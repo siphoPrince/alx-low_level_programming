c90f7237a67cc2e4490016785cb6d84d18ba2da5
Sipho <mabirimisesipho@gmail.com>
2023-02-27 02:49:37 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaping intitial values
+ * @a: first integer
+ * @b: second integer
+ * Return: 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*b = i;
+}
