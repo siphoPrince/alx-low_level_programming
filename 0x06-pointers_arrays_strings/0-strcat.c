498a12ce14b874624479ec8000031e537333c5bf
Sipho <mabirimisesipho@gmail.com>
2023-03-01 10:00:15 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,19 @@
  * Return: A pointer to the resulting string dest
 */
 
-char *_strcat(char *dest, char *src, int m)
+char *_strcat(char *dest, char *src, int )
 {
 
-	strncat(dest, src, m);
+	int len = 0;
+	int len = i;
+
+	while (dest[len)
+		len++;
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
