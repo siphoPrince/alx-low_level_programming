27ae01ad20b7acd86ab741e9966230a4280c412b
Sipho <mabirimisesipho@gmail.com>
2023-03-02 07:05:33 -0800

    capital 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+/**
+ * cap_string - funvtion used
+ * str: parr used to pass values
+ */
+
+char *cap_string(char *str)
+{
+	int len = strlen(str);
+	int i;
+
+	if (len > 0 && islower(str[0]))
+	{
+	str[0] = toupper(str[0]);
+	}
+		for (i = 1; i < len; i++)
+		{
+		if (isspace(str[i - 1]) 
+			|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' ||
+			str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+			{
+				if (islower(str[i]))
+				{
+				str[i] = toupper(str[i]);
+			}
+		}
+	}
+
+	return (str);
+}
+
