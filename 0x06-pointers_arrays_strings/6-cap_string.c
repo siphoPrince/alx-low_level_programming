e0f6928bd5470962a5e1104db41624e6984ba309
Sipho <mabirimisesipho@gmail.com>
2023-03-02 08:28:11 -0800

    6 caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,6 @@
  * @str: parr used to pass values
  * Return: str
 */
-
 char *cap_string(char *str)
 {
 	int len = strlen(str);
@@ -14,20 +13,22 @@ char *cap_string(char *str)
 
 	if (len > 0 && islower(str[0]))
 	{
-	str[0] = toupper(str[0]);
+		str[0] = toupper(str[0]);
 	}
-	for (i = 1; i < len; i++)
 
-	if (isspace(str[i - 1])
-	|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-	str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' ||
+	for (i = 1; i < len; i++)
+	{
+	if (isspace(str[i - 1]) || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || 
+	str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || 
 	str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
 	{
-		if (islower(str[i]))
-		{
-		str[i] = toupper(str[i]);
-		}
-
-		return (str);
+	if (islower(str[i])) 
+	{
+	str[i] = toupper(str[i]);
 	}
+	}
+	}
+
+	return (str);
 }
+
