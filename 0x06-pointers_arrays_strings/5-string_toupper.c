909abf0a44153f59a28faef5fe86bb6bb119eadf
Sipho <mabirimisesipho@gmail.com>
2023-03-02 10:06:03 -0800

    string upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,15 +7,13 @@
 
 char *string_toupper(char *str)
 {
-	char *p = str;
+	int i = 0;
 
-	while (*p != '\0')
+	while (str[i])
 	{
-		if (islower(*p))
-		{
-			*p = toupper(*p);
-		}
-			p++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
 	}
 	return (str);
 }
