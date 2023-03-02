78b7c1f307ab7e856c8ee9aa2777087b02a7c0cd
Sipho <mabirimisesipho@gmail.com>
2023-03-02 02:34:09 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to
+ * @dest: the destiation string to be concatenated
+ * @n: input value
+ * Return: the value of dest
+*/
+char *_strcat(char *dest, char *src, int n)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
