d942485c1e415745d63f421348f197d00c9e2d00
Sipho <mabirimisesipho@gmail.com>
2023-03-06 08:25:05 -0800

    7 print

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i1, i2;
+	int indx1, indx2;
 
-	for (i1 = 0; a[i1][7]; i1++)
+	for (indx1 = 0; a[indx1][7]; indx1++)
 	{
-		for (i2 = 0; i2 < 8; i2++)
-			_putchar(a[i1][i2]);
+		for (indx2 = 0; indx2 < 8; indx2++)
+			_putchar(a[indx1][indx2]);
 
 		_putchar('\n');
 	}
