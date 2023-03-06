34091092dbc25364c414295a705f84bbf3e06848
Sipho <mabirimisesipho@gmail.com>
2023-03-06 06:55:15 -0800

    7-main.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - main function
+ * @a: parr used
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (i < 8)
+	{
+	j = 0;
+	while (j < 8)
+	{
+	printf("%c ", a[i][j]);
+	j++;
+	}
+	printf("\n");
+	i++;
+	}
+}
