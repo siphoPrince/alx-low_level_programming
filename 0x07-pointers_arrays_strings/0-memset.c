9d668db7845a4ecde64400635bc9e1c870cf6ed4
Sipho <mabirimisesipho@gmail.com>
2023-03-06 01:43:52 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memse - the main function 
+ * @s: first parr
+ * @b:second par
+ * @n:int parr
+ * Return: alwasy 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+	while (n--)
+	{
+		*p++ = b;
+	}
+	return (s);
+		
+}
