af002b269f3a5af924481703cdc7bf60817ee101
Sipho <mabirimisesipho@gmail.com>
2023-03-06 03:02:14 -0800

    memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - main function
+ * @dest: 1st parr
+ * @src: second parr
+ * @n: unasigned parr
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dn = dest;
+	char *s = src
+
+	while (n--)
+	{
+		*dn++ = *s++;
+	}
+
+	return (dest);
+}
