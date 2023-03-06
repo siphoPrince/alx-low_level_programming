adc080a23e944bf30a28490d4368d6975b2aa025
Sipho <mabirimisesipho@gmail.com>
2023-03-06 09:12:58 -0800

    7 print

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
 #include "main.h"
-#include "_putchar.c"
-
 /**
- * print_chessboard - main function
- * @a: parr used
- * Return: 0
+ * print_chessboard - Entry point
+ * @a: array
+ * Return:0
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i;
