692f489b238c4a62c77aaa1709f34af9e3c6e771
Sipho <mabirimisesipho@gmail.com>
2023-03-08 02:58:19 -0800

    4-pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns value of x and y
+ * @x: first input
+ * @y: second input
+ * Return: value of x and y
+ */
+
+int _pow_recursion(x, y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x  _pow_recursion(x, y - 1));
+}
