3b863d3fa094f98d04dcb5a978f1bba7e3cfd6e2
Sipho <mabirimisesipho@gmail.com>
2023-03-08 01:41:56 -0800

    0-putss_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,10 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		putchar(*s);
-		_puts_recursion(s + 1);
+		putchar('\n');
+		return;
+
 	}
-	return;
-
-	putchar('\n');
-
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
