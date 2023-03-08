ead0e6cfde3772be69b70c0bed75daeb84b58d8d
Sipho <mabirimisesipho@gmail.com>
2023-03-08 02:14:03 -0800

    1-print_rev_rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - function to print in reverse
+ * @s: input
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		return;
+	}
+		_print_rev_recursion(s + 1);
+
+		putchar(*s);
+}
