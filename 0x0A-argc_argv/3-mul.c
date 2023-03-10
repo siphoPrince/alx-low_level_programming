a2e8e17dab2fb3e57d5048cd482decd1fd724759
Sipho <mabirimisesipho@gmail.com>
2023-03-10 08:52:44 -0800

    3 multipl

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int sum1 = atoi(argv[1]);
-	int sum2 = atoi(argv[2]);
-	int result = sum1 * sum2;
+	int sum1, sum2, result;
 
 	if (argc != 3)
 	{
 	printf("Error\n");
 	return (1);
 	}
+
+	sum1 = atoi(argv[1]);
+	sum2 =atoi(argv[2]);
+	result = sum1 * sum2;
+
 
 	printf("%d\n", result);
 	return (0);
