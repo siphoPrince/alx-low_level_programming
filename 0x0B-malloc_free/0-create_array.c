8730af02148406c287c487bc8620d30719966fa4
Sipho <mabirimisesipho@gmail.com>
2023-03-13 05:19:21 -0700

    create_arry

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_arry - creates an arry
+ * @size: size used 
+ * @c: special char used
+ * Return: special charr
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr = malloc(size * sizeof(char));
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
