133b69ca5226d50e7ddb883e6b67d96ccc2e8230
siphoPrince <mabirimisesipho@gmail.com>
2023-05-04 09:45:45 -0700

    code fix

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: Pointer to a pointer to the head node of the list.
+ * @n: Integer value to store in the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *last_node;
+
+	/* create a new node */
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	/* if the list is empty, set the head to the new node */
+	if (*head == NULL)
+	{
+	*head = new_node;
+	return (new_node);
+	}
+
+	/* find the last node */
+	last_node = *head;
+	while (last_node->next != NULL)
+	last_node = last_node->next;
+
+	/* append the new node to the end */
+	last_node->next = new_node;
+
+	return (new_node);
+}
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
