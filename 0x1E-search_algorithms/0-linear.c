19fe52a71fdd54a4ee7ea0608c826785dc195822
siphoPrince <mabirimisesipho@gmail.com>
2023-12-12 01:10:38 -0800

    printing in linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * linear_search - Search for a value in an array using Linear search algorithm
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: The value to search for
+ *
+ * Return: The first index where the value is located,
+ *         or -1 if the value is not present in the array or if the array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
